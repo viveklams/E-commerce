c268bbbd2e979cbaa69593de1ca91172d2325344
viveklams <viveklamgadey@gmail.com>
2024-09-11 16:22:27 +0530

    a middleware function has been provided for authentication called protectedRoute

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+import { jwt } from "jsonwebtoken";
+import User from "../models/user.model";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken) {
+      res
+        .status(400)
+        .json({ message: "Unauthorized - No access token provided" });
+    }
+
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId).select("-password");
+
+      if (!user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "UnAuthorized - Access Token expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("Error in protectedRoute middleware", error.message);
+    return res
+      .status(401)
+      .json({ message: "Unathourized - Invalid access token" });
+  }
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAllProducts } from "../controllers/productController";
+import { protectRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+router.get("/", protectRoute, getAllProducts);
 
 export default router;
