1add384b54ddfb460e1b05c9443b7e50c82950bd
viveklams <viveklamgadey@gmail.com>
2024-09-12 14:10:41 +0530

    backend logic for getRecommendations added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -90,3 +90,27 @@ export const deleteProduct = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const getRecommendations = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 3 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    console.log("Error in getRecommendations controller", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
