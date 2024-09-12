dca0bfbaa1327effaacb67b7417ffe6b7508c168
viveklams <viveklamgadey@gmail.com>
2024-09-12 11:17:53 +0530

    made some changes in getFeaturedProfucts function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import { redis } from "./../lib/redis.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -6,6 +7,30 @@ export const getAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.log("Error in getAllProducts controller", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    //if it is not from redis , fetch it from mongodb
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    // store in the redis for future quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller", error.message);
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllProducts } from "../controllers/productController.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/productController.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
