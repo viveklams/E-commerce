7d94e0b58e12827eaa5f6d5d44f756644dfc3194
viveklams <viveklamgadey@gmail.com>
2024-09-12 12:40:16 +0530

    dont with createProduct with cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -34,3 +34,32 @@ export const getFeaturedProducts = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+
+    let cloudinaryResponse = null;
+
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.log("Error in createProduct controller", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
