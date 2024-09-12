1a0efa2994a1bbb93619fc633771bf5ab2d818d5
viveklams <viveklamgadey@gmail.com>
2024-09-12 11:54:39 +0530

    cloudinary configuration added for image upload

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -9,5 +9,6 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 export default router;
