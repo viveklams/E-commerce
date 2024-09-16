c0dbd433d4cefe0de637ad54f7ffd6d9a1c2c632
viveklams <viveklamgadey@gmail.com>
2024-09-16 19:01:23 +0530

    cart controller logic with get, delete, remove, inc-dec-quantity, added

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 
-import { addToCart } from "../controllers/cartController.js";
+import {
+  addToCart,
+  removeAllFromCart,
+  updateQuantity,
+} from "../controllers/cartController.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
