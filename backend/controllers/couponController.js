e319d846233af00185f1767f4176114116517a13
viveklams <viveklamgadey@gmail.com>
2024-09-17 13:51:21 +0530

    function made for couponValidation -> couponValidation & updation at the routes file too

--- backend/controllers/couponController.js
+++ backend/controllers/couponController.js
@@ -12,3 +12,33 @@ export const getCoupon = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const coupon = await Coupon.findOne({
+      code: code,
+      userId: req.user._id,
+      isActive: true,
+    });
+
+    if (!coupon) {
+      return res.status(400).json({ message: "Coupon not found" });
+    }
+
+    if (coupon.expirationDate < new Date()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(404).json({ message: "Coupon expired" });
+    }
+
+    res.json({
+      message: "Coupon is valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log("Error in validateCoupon controller", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
