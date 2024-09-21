c6f56a7ead99c8408f0c457f8337a8bf12dc8390
viveklams <viveklamgadey@gmail.com>
2024-09-21 13:10:46 +0530

    payment route added to backend on server.js file

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 //database connection
 import { connectDB } from "./lib/db.js";
 //call the dotenv function
@@ -29,6 +30,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/carts", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
