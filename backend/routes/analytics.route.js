99d4503b90a71797bc7d32b5f73bef94740fdeb4
viveklams <viveklamgadey@gmail.com>
2024-09-23 15:53:35 +0530

    server file added

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import { getAnalyticsData } from "../controllers/analytics.controller.js";
+import {
+  getAnalyticsData,
+  getDailySalesData,
+} from "../controllers/analytics.controller.js";
 
 const router = express.Router();
 
