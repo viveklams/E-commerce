0dbe2db027133a2f22f2b2fa6a66af93fdafc2c1
viveklams <viveklamgadey@gmail.com>
2024-09-24 14:35:41 +0530

    imported the necessary oackages for SignUpPage like useState, Link, tools in Lucide_react , framer-motion for animation

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import { motion } from "framer-motion";
 const SignUpPage = () => {
   return (
     <div>
