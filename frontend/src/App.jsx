56ac0283e49cb9e8fd728669eac51be9ddda6e85
viveklams <viveklamgadey@gmail.com>
2024-09-24 13:25:37 +0530

    modified on app.jsx and navbar component modified -> design and logic for user & admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,15 @@ function App() {
           <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
         </div>
       </div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
