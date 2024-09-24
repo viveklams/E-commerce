445b690837e7e0d5f1f35445bead55aa89c9fba6
viveklams <viveklamgadey@gmail.com>
2024-09-24 14:56:55 +0530

    modification on return statement for SignUpPage -> features added = <motion.div>(animation purposes),and started with <form>

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -11,13 +11,30 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
+  //handle Submit Function
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
   };
   return (
-    <div>
-      <h2>SignUpPage</h2>
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        Create your account
+      </motion.div>
+
+      <motion.div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10"></div>
+      </motion.div>
     </div>
   );
 };
