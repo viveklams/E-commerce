51ba2c4099ec2d893516358887ebd0448c2446c4
viveklams <viveklamgadey@gmail.com>
2024-09-24 16:44:42 +0530

    added the logic for if the user is logged In, and navigation logic to Login page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -24,7 +24,9 @@ const SignUpPage = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        Create your account
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">
+          Create your account
+        </h2>
       </motion.div>
 
       <motion.div
@@ -146,7 +148,40 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent 
+							rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600
+							 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+							  focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Loader
+                    className="mr-2 h-5 w-5 animate-spin"
+                    aria-hidden="true"
+                  />
+                  Loading...
+                </>
+              ) : (
+                <>
+                  <UserPlus className="mr-2 h-5 w-5" aria-hidden="true" />
+                  Sign up
+                </>
+              )}
+            </button>
           </form>
+
+          <p className="mt-8 text-center text-sm text-gray-400">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="font-medium text-emerald-400 hover:text-emerald-300"
+            >
+              Login here <ArrowRight className="inline h-4 w-4" />
+            </Link>
+          </p>
         </div>
       </motion.div>
     </div>
