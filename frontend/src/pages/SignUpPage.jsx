df74ea8ac05c8a90343a66b7587f051f2a48e460
viveklams <viveklamgadey@gmail.com>
2024-09-24 14:42:01 +0530

    created useState array with two elements [fomrData, setFormData] and created a handleSubmit function

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,18 @@ import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
 const SignUpPage = () => {
+  const loading = true;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <div>
       <h2>SignUpPage</h2>
