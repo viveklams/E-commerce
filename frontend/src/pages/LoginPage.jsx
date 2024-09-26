97e5db33a205a532b10a3442f9b1c51b28dfe79e
viveklams <viveklamgadey@gmail.com>
2024-09-26 23:15:22 +0530

    styling for product page completed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         transition={{ duration: 0.8 }}
       >
         <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">
-          Create your account
+          Login
         </h2>
       </motion.div>
 
