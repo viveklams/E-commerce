813f17921e47c18807ced29ae38707acee53fdac
viveklams <viveklamgadey@gmail.com>
2024-09-27 14:37:14 +0530

    fetch all products added in the useProductStore

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -21,4 +21,25 @@ export const useProductStore = create((set) => ({
       set({ loading: false });
     }
   },
+  fetchAllProducts: async () => {
+    set({ loading: true });
+    try {
+      const response = await axios.get("/products");
+      set({ products: response.data.products, loading: false });
+    } catch (error) {
+      set({ error: "Failed to fetch products", loading: false });
+      toast.error(error.response.data.error || "Failed to fetch products");
+    }
+  },
+  deleteProduct: async (id) => {
+    set({ loading: true });
+    try {
+      const response = await axios.get(`/products/category/${category}`);
+      set({ products: response.data.products, loading: false });
+    } catch (error) {
+      set({ error: "Failed to fetch products", loading: false });
+      toast.error(error.response.data.error || "Failed to fetch products");
+    }
+  },
+  toggleFeaturedProduct: async (id) => {},
 }));
