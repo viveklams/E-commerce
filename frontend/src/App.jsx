ffb9066ca448ed7c4c803b8f4b8d69b83c4d0c27
viveklams <viveklamgadey@gmail.com>
2024-09-30 23:12:24 +0530

    designings for cartpage has been made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,20 @@ import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import { useCartStore } from "./stores/useCartStore";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
+  const { getCartItems } = useCartStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    getCartItems();
+  }, [getCartItems]);
 
   if (checkingAuth) return <LoadingSpinner />;
   return (
@@ -47,6 +54,11 @@ function App() {
           />
 
           <Route path="/category/:category" element={<CategoryPage />} />
+
+          <Route
+            path="/cart"
+            element={user ? <CartPage /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
       <Toaster />
