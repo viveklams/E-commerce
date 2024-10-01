d95374e90e1f6627a3050bc62d02d4eb36ef41fd
viveklams <viveklamgadey@gmail.com>
2024-10-01 14:47:36 +0530

    Order Summary design started upto coupon integration

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -1,5 +1,48 @@
+import { motion } from "framer-motion";
+import { useCartStore } from "../stores/useCartStore";
+
 const OrderSummary = () => {
-  return <div>Order Summary</div>;
+  const { total, subtotal, coupon, isCouponApplied } = useCartStore();
+
+  const savings = subtotal - total;
+  const formattedSubtotal = subtotal.toFixed(2);
+  const formattedTotal = total.toFixed(2);
+  const formattedSavings = savings.toFixed(2);
+
+  return (
+    <motion.div
+      className="space-y-4 rounded-lg border border-gray-700 bg-gray-800 p-4 shadow-sm sm:p-6"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <p className="text-xl font-semibold text-emerald-400">Order Summary</p>
+
+      <div className="space-y-4">
+        <div className="space-y-4">
+          <dl className="flex items-center justify-between gap-4">
+            <dt className="text-base font-normal text-gray-300">
+              Original price
+            </dt>
+            <dd className="text-base font-medium text-white">
+              ${formattedSubtotal}
+            </dd>
+          </dl>
+
+          {savings > 0 && (
+            <dl className="flex items-center justify-between gap-4">
+              <dt className="text-base font-normal text-gray-300">Savings</dt>
+              <dd className="text-base font-medium text-emerald-400">
+                -${formattedSavings}
+              </dd>
+            </dl>
+          )}
+
+          {coupon}
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default OrderSummary;
