718dd5ac48f64f38d206938b054b8ff0de4f164b
viveklams <viveklamgadey@gmail.com>
2024-10-02 07:29:51 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 //routes
 import authRoutes from "./routes/auth.route.js";
@@ -19,6 +20,8 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 console.log(process.env.PORT);
 
 //allows you to parse body of the request
@@ -39,6 +42,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
