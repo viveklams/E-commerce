6640b0c3b6b17ddafb7316c3799b1540275d4427
viveklams <viveklamgadey@gmail.com>
2024-10-03 05:21:07 +0530

    changes made in ProductCard

--- frontend/src/components/PeopleAlsoBought.jsx
+++ frontend/src/components/PeopleAlsoBought.jsx
@@ -33,7 +33,7 @@ const PeopleAlsoBought = () => {
       <h3 className="text-2xl font-semibold text-emerald-400">
         People also bought
       </h3>
-      <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg: grid-col-3">
+      <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {recommendations.map((product) => (
           <ProductCard key={product._id} product={product} />
         ))}
@@ -41,4 +41,5 @@ const PeopleAlsoBought = () => {
     </div>
   );
 };
+
 export default PeopleAlsoBought;
