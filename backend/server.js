c66a7e09c99d40b455e1019516cfd843150e5830
viveklams <viveklamgadey@gmail.com>
2024-10-03 04:44:23 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
+
 console.log(process.env.PORT);
 
 //allows you to parse body of the request
@@ -30,7 +31,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from your frontend
+    origin: "http://localhost:3000", // Allow requests from your frontend
   })
 );
 
